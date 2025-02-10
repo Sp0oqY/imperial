b0fe25f90831c32e0ee4d05088537a6c367d9fc3
MiroslavKastier <272835@vutbr.cz>
2025-02-10 23:31:56 +0100

    Upravil som hlavičku aby bola responzívna, treba pridať ešte logo a upraviť v css

--- includes/header.php
+++ includes/header.php
@@ -1,50 +1,17 @@
-<?php
-if (session_status() === 1) {
-    session_start();
-}
-
-?>
-<header class="header">
-    <!--responsive navbar-->
+<nav>
     <input type="checkbox" id="check">
     <label for="check" class="checkbtn">
         <i class="fas fa-bars"></i>
     </label>
-
-    <!--Title / logo-->
-    <div class="title">
-        <h2>Imperial Shoes & Clothes</h2>
-
-        <div class="buttons">
-
-            <?php if (isset($_SESSION['id'])) : ?>
-                <a href="/rocnikovy/logout"><button class="Login">Logout</button></a>
-            <?php else : ?>
-                <a href="/rocnikovy/login"><button class="Login">Login</button></a>
-                <a href="/rocnikovy/signup"><button class="SignUp">Sign Up</button></a>
-            <?php endif; ?>
-
-            <div class="dropdown">
-                <a class="settings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="https://freesvg.org/storage/img/thumb/gearRed.png" alt="">
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right mt-10" aria-labelledby="dropdownMenuButton" style="top: 15px;">
-                    <a class="dropdown-item" href="/rocnikovy/settings">Zmnena údajov</a>
-                    <a class="dropdown-item" href="/rocnikovy/settings">Zmena hesla</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--login / logout buttons -->
-    <!--navbar-->
-    <ul class="navigation">
-        <li><a id="activeH" class="navBtn" href="/index.php">Home</a></li>
-        <li><a id="activeA" class="navBtn" href="/about.php">About</a></li>
-        <li><a id="activeL" class="navBtn" href="/list.php">List</a></li>
-        <li><a id="activeC" class="navBtn" href="/contact.php">Reviews</a></li>
-        <li><a id="activeS" class="navBtn" href="/support.php">Support</a></li>
-        <li><a id="activeC" class="navBtn" href="/contact.js">Contact</a></li>
+    <label class="logo">Imperial Shoes & Clothes</label>
+    <ul>
+        <li><a class="active" href="#">Domov</a></li>
+        <li><a href="#">O nás</a></li>
+        <li><a href="#">Katalóg</a></li>
+        <li><a href="#">Recenzie</a></li>
+        <li><a href="#">Kontakt</a></li>
     </ul>
-</header>
+</nav>
+<section>
+
+</section>
--- index.php
+++ index.php
@@ -7,47 +7,7 @@ require './Db.php';
 
 Db::connect('sql.endora.cz:3313', 'rocnikovy', 'imperialshoes', 'RLQ7Kroy2aYn1xDo');
 
-// try 
-// {
-//     $pdo = new PDO("mysql:host=localhost;dbname=imperial;charset=utf8", "root", "");
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } 
-// catch (PDOException $e) 
-// {
-//     die("Chyba pripojenia: " . $e->getMessage());
-// }
-
-if (isset($_GET['page'])) 
-{
-    $page = $_GET['page'];
-    if (is_int($page) || $page != 1) 
-    {
-        $offset = ($page - 1) * 3;
-        $news = Db::queryAll("SELECT * FROM `news` ORDER BY `date` DESC LIMIT 3 OFFSET ${offset};");
-    } 
-
-    else 
-    {
-        header("Location: /imperial/");
-    }
-} 
-else 
-{
-    $news = Db::queryAll("SELECT * FROM `news` ORDER BY `date` DESC LIMIT 3;");
-}
-
-$count = Db::query("SELECT * FROM `news`");
-if ($count % 2 == 0) 
-{
-    $count = ($count / 2) - 1;
-} 
-
-else 
-{
-    $count = $count / 2;
-}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -56,30 +16,10 @@ else
     <?php require_once "./includes/head.php" ?>
 </head>
 <body>
-    <section id="pic">
+    <section>
         <?php require_once "./includes/header.php" ?>
-        <div class="row">
-            <div class="col-12 content">
-            <div class="content-news">
-                    <h1>News</h1>
-
-                    <?php foreach ($news as $item) : ?>
-                        <h4><?= $item['title'] ?></h4>
-                        <img id="imgLeft" src="<?= $item['image'] ?>" alt="">
-                        <p> <?= $item['description'] ?> </p>
-                        <small><?= date("d.m.Y", strtotime($item['date'])) ?></small>
-                    <?php endforeach; ?>
-
-                    <section class="page">
-                        <?php for ($i = 1; $i <= $count; $i++) : ?>
-                            <a href="/rocnikovy?page=<?= $i ?>"><?= $i ?></a>
-                        <?php endfor; ?>
-                    </section>
-        </div>
-            </div>
-        </div>
         
-
+    
         <?php require_once "./includes/footer.php" ?>
     </section>
 </body>
