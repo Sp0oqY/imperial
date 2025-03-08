3919f3e033c67f6f4d759e671fd438b59055375d
Sp0oqY <kastierm45@gmail.com>
2025-03-09 00:29:54 +0100

    začal som s úpravou contact formu

--- contact.php
+++ contact.php
@@ -9,6 +9,7 @@
 
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
     <script src="./js/contact.js"></script>
+    
     <?php require_once "./includes/head.php" ?>
 </head>
 
@@ -17,39 +18,42 @@
     <section>
         <form class="container" onsubmit="sendMail(event)">
             <div class="contact-box">
-                <div class="left">
-                    <h2>Contact Form</h2>
-                    <p>Username</p>
-                    <input type="text" id="name" class="field" placeholder="Enter your name" required />
-                    <p>E-mail</p>
-                    <input type="email" id="email" class="field" placeholder="Enter your email" required />
-                    <p>Subject</p>
-                    <input type="text" id="subject" class="field" placeholder="Enter subject" required />
-                    <p>Message</p>
-                    <textarea id="message" class="field area" placeholder="Enter message" required></textarea>
+                <h2>Kontaktný Formulár</h2>
+                <p>Meno</p>
+                <input type="text" id="name" class="field" placeholder="Zadajte svoje meno" required />
+                <p>E-mail</p>
+                <input type="email" id="email" class="field" placeholder="Zadajte svoj e-mail" required />
+                <p>Predmet</p>
+                <input type="text" id="subject" class="field" placeholder="Zadajte predmet" required />
+                <p>Správa</p>
+                <textarea id="message" class="field area" placeholder="Zadajte správu" required></textarea>
 
-                    <button class="btn" type="submit">Send</button>
+                <button class="btn" type="submit">Odoslať</button>
+
+                <div class="network">
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100008982675113">
+                        <img class="net fb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/2048px-2023_Facebook_icon.svg.png" alt="Facebook" />
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/miroslavkastier/?hl=sk">
+                        <img class="net ig" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" alt="Instagram" />
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miroslav-ka%C5%A1tier-788a92310/">
+                        <img class="net" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1200px-LinkedIn_icon.svg.png" alt="LinkedIn" />
+                    </a>
                 </div>
-
-                <div class="right">
-                    <h2>Contact Info</h2>
-                    <ul>
-                        <li>MK</li>
-                        <li>mk@gmail.com <br /> +421 000 000 000</li>
-                        <li>Masarykova Univerzita <br /> Fakulta Informatiky</li>
-                        <li>Botanická 68A, 602 00 <br /> Královo Pole-Ponava, Brno</li>
-                    </ul>
-
-                    <div class="network">
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100008982675113">
-                            <img class="net fb" src="https://image.similarpng.com/very-thumbnail/2020/04/Beautiful-Facebook-logo-icon-social-media-png.png" alt="Facebook" />
-                        </a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/miroslavkastier/?hl=sk">
-                            <img class="net ig" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" alt="Instagram" />
-                        </a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miroslav-ka%C5%A1tier-788a92310/">
-                            <img class="net" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1200px-LinkedIn_icon.svg.png" alt="LinkedIn" />
-                        </a>
+            </div>
+            <div class="support">
+                <h1>FREQUENTLY ASKED QUESTIONS</h1>
+                <div class="accordion">
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                            <p>Otázka</p>
+                        </div>
+                        <div class="accordion-item-body">
+                            <div class="accordion-item-body-content">
+                                <p>Odpoveď</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -57,6 +61,7 @@
     </section>
 
     <?php require_once "./includes/footer.php" ?>
+    <script src="./js/support.js"></script>
 </body>
 
 </html>
