23af1142801f0dd2d7f27bf7020a765e0068cd57
Sp0oqY <kastierm45@gmail.com>
2025-03-12 23:46:13 +0100

    upravil som hlavičku, len treba ešte doladiť logo

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,11 @@
             <i class="fas fa-bars"></i>
         </label>
         <ul>
-            <li><a href="/imperial/index.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/index.php") ? "active" : ""; ?>">Domov</a></li>
-            <li><a href="/imperial/about.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/about.php") ? "active" : ""; ?>">O nás</a></li>
-            <li><a href="/imperial/catalog.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/catalog.php") ? "active" : ""; ?>">Katalóg</a></li>
-            <li><a href="/imperial/reviews.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/reviews.php") ? "active" : ""; ?>">Recenzie</a></li>
-            <li><a href="/imperial/contact.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/contact.php") ? "active" : ""; ?>">Kontakt</a></li>
+            <li><a href="/imperial/index.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/index.php") ? "active" : "navHover"; ?>">Domov</a></li>
+            <li><a href="/imperial/about.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/about.php") ? "active" : "navHover"; ?>">O nás</a></li>
+            <li><a href="/imperial/catalog.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/catalog.php") ? "active" : "navHover"; ?>">Katalóg</a></li>
+            <li><a href="/imperial/reviews.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/reviews.php") ? "active" : "navHover"; ?>">Recenzie</a></li>
+            <li><a href="/imperial/contact.php" class="<?= ($_SERVER['PHP_SELF'] == "/imperial/contact.php") ? "active" : "navHover"; ?>">Kontakt</a></li>
         </ul>
 
         <div class="logo">
