edef308067db90d9a27fefafbc4dd18c50cbf4e4
MiroslavKastier <272835@vutbr.cz>
2025-03-13 14:29:55 +0100

    začal som upravovať kontakt form a support

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ require './Db.php';
 //Db::connect('sql.endora.cz:3313', 'imperial', 'imperialshoes', 'RLQ7Kroy2aYn1xDo');
 Db::connect('localhost', 'imperial', 'root', '');
 
-$questions = Db::queryAll("SELECT * FROM faq");
+$questions = Db::queryAll("SELECT * FROM faq LIMIT 6");
 
 ?>
 
@@ -32,20 +32,21 @@ $questions = Db::queryAll("SELECT * FROM faq");
     <?php require_once "./includes/header.php" ?>
     <section>
         <div class="container">
-            <form class="contact-box" onsubmit="sendMail(event)">
-                <h2>Kontaktný Formulár</h2>
-                <p>Meno</p>
-                <input type="text" id="name" class="field" placeholder="Zadajte svoje meno" required />
-                <p>E-mail</p>
-                <input type="email" id="email" class="field" placeholder="Zadajte svoj e-mail" required />
-                <p>Predmet</p>
-                <input type="text" id="subject" class="field" placeholder="Zadajte predmet" required />
-                <p>Správa</p>
-                <textarea id="message" class="field area" placeholder="Zadajte správu" required></textarea>
+            <div class="wrapper">
+                <form class="contact-box" onsubmit="sendMail(event)">
+                    <h2>Kontaktný Formulár</h2>
+                    <p>Meno</p>
+                    <input type="text" id="name" class="field" placeholder="Zadajte svoje meno" required />
+                    <p>E-mail</p>
+                    <input type="email" id="email" class="field" placeholder="Zadajte svoj e-mail" required />
+                    <p>Predmet</p>
+                    <input type="text" id="subject" class="field" placeholder="Zadajte predmet" required />
+                    <p>Správa</p>
+                    <textarea id="message" class="field area" placeholder="Zadajte správu" required></textarea>
 
-                <button class="btn" type="submit">Odoslať</button>
+                    <button class="btn" type="submit">Odoslať</button>
 
-                <!-- <div class="network">
+                    <!-- <div class="network">
                     <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100008982675113">
                         <img class="net fb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/2048px-2023_Facebook_icon.svg.png" alt="Facebook" />
                     </a>
@@ -56,22 +57,23 @@ $questions = Db::queryAll("SELECT * FROM faq");
                         <img class="net" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1200px-LinkedIn_icon.svg.png" alt="LinkedIn" />
                     </a>
                 </div> -->
-            </form>
-            <div class="support">
-                <h1>FREQUENTLY ASKED QUESTIONS</h1>
-                <div class="accordion">
-                    <?php foreach ($questions as $question): ?>
-                        <div class="accordion-item">
-                            <div class="accordion-item-header">
-                                <p><?= $question['question'] ?></p>
-                            </div>
-                            <div class="accordion-item-body">
-                                <div class="accordion-item-body-content">
-                                    <p><?= $question['answer'] ?></p>
+                </form>
+                <div class="support">
+                    <h2>NAJČASTEJŠIE OTÁZKY</h2>
+                    <div class="accordion">
+                        <?php foreach ($questions as $question) : ?>
+                            <div class="accordion-item">
+                                <div class="accordion-item-header">
+                                    <p><?= $question['question'] ?></p>
+                                </div>
+                                <div class="accordion-item-body">
+                                    <div class="accordion-item-body-content">
+                                        <p><?= $question['answer'] ?></p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endforeach ?>
+                        <?php endforeach ?>
+                    </div>
                 </div>
             </div>
         </div>
