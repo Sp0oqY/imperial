7391f0c8f6b023a3b53302b884cfbba8caf7e82d
Sp0oqY <kastierm45@gmail.com>
2025-03-15 18:44:08 +0100

    upravil som media v contact, treba to ešte doladiť v css neskor a vyriešiť contact.js

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ require './Db.php';
 //Db::connect('sql.endora.cz:3313', 'imperial', 'imperialshoes', 'RLQ7Kroy2aYn1xDo');
 Db::connect('localhost', 'imperial', 'root', '');
 
-$questions = Db::queryAll("SELECT * FROM faq LIMIT 6");
+$questions = Db::queryAll("SELECT * FROM faq");
 
 ?>
 
@@ -20,11 +20,7 @@ $questions = Db::queryAll("SELECT * FROM faq LIMIT 6");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/contact.css">
-    <title>Contact Form</title>
-
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
-    <script src="./js/contact.js"></script>
-
     <?php require_once "./includes/head.php" ?>
 </head>
 
@@ -33,8 +29,8 @@ $questions = Db::queryAll("SELECT * FROM faq LIMIT 6");
     <section>
         <div class="container">
             <div class="wrapper">
-                <form class="contact-box" onsubmit="sendMail(event)">
-                    <h2>Kontaktný Formulár</h2>
+                <form class="contact-box" >
+                    <h2>KONTAKTNÝ FORMULÁR</h2>
                     <p>Meno</p>
                     <input type="text" id="name" class="field" placeholder="Zadajte svoje meno" required />
                     <p>E-mail</p>
@@ -44,20 +40,9 @@ $questions = Db::queryAll("SELECT * FROM faq LIMIT 6");
                     <p>Správa</p>
                     <textarea id="message" class="field area" placeholder="Zadajte správu" required></textarea>
 
-                    <button class="btn" type="submit">Odoslať</button>
-
-                    <!-- <div class="network">
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100008982675113">
-                        <img class="net fb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/2048px-2023_Facebook_icon.svg.png" alt="Facebook" />
-                    </a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/miroslavkastier/?hl=sk">
-                        <img class="net ig" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" alt="Instagram" />
-                    </a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/miroslav-ka%C5%A1tier-788a92310/">
-                        <img class="net" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1200px-LinkedIn_icon.svg.png" alt="LinkedIn" />
-                    </a>
-                </div> -->
+                    <button class="btn" onsubmit="sendMail(event)" type="submit">ODOSLAŤ</button>
                 </form>
+
                 <div class="support">
                     <h2>NAJČASTEJŠIE OTÁZKY</h2>
                     <div class="accordion">
@@ -79,8 +64,9 @@ $questions = Db::queryAll("SELECT * FROM faq LIMIT 6");
         </div>
     </section>
 
-    <!-- <?php require_once "./includes/footer.php" ?> -->
+    <?php require_once "./includes/footer.php" ?> 
     <script src="./js/support.js"></script>
+    <script src="./js/contact.js"></script>
 </body>
 
 </html>
